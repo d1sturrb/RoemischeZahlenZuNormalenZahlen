1339e53c7ec6fd3ed612cff7a7439ec48bb5cebd
philippti <philippt@dhbw-loerrach.de>
2020-10-30 11:51:45 +0100

    Fertiges Programm (Timo)

--- romanNumbers.py
+++ romanNumbers.py
@@ -1,3 +1,4 @@
+# Dictionnary with all possible roman numbers
 numdict = {
         "I" : 1,
         "V" : 5,
@@ -8,24 +9,26 @@ numdict = {
         "M" : 1000
 }
 
+# save input of user as sting
 romNum = str
 romNum = str(input("Römische nummer eingeben: "))
 
+#translate the roman number to decimal
 def translate(romNum):
     sum = 0
     prev_num = 0
-    for char in romNum:
-        if char in numdict:
-            if numdict[char] > prev_num:
-                sum -= prev_num
+    for char in romNum:                     # loop over every character in the roman number
+        if char in numdict:                 # check if character is in the dictionnary
+            if numdict[char] > prev_num:    # if the found roman number is larger than the previous number, substract it (case: IX)
+                sum -= prev_num             # substract from total sum
             else:
-                sum += prev_num
-        prev_num = numdict[char]
-    sum += prev_num
+                sum += prev_num             # else: add to the previous number (case: XI)
+        prev_num = numdict[char]            # update the previous number to the current number for the next iteration on the string
+    sum += prev_num                         # add the previous number to the total sum
 
     return sum
 
-print("Dezimalzahl: " , translate(romNum))
+print("Dezimalzahl: " , translate(romNum))  # print out the result
 
 
 
