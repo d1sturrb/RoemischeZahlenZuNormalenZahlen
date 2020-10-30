0ac6eb18eea0ff6bdb93766acc3e36c2c0c8f522
d1sturrb <alexander.biber@outlook.com>
2020-10-30 11:08:47 +0100

    alex - 1

---
+++ alex.py
@@ -0,0 +1,19 @@
+"""
+Schreiben Sie ein Python-Programm, das eine beliebige römische Zahl in eine
+„gewöhnliche” Dezimalzahl umrechnet.
+"""
+
+r_I = 1
+r_V = 5
+r_X = 10
+r_L = 50
+r_C = 100
+r_D = 500
+r_M = 1000
+
+
+if __name__ == "__main__":
+    input = input("Roemische Zahl eingeben:  ")
+    print(input)
+    
+    
