f056167ae4811c54f9e59ab977a41681fe39cf73
d1sturrb <alexander.biber@outlook.com>
2020-11-01 18:47:28 +0100

    finished with help of Darkestry's code

--- alex.py
+++ alex.py
@@ -1,19 +1,26 @@
 """
+S.83 (102/558) - N10.6
+
 Schreiben Sie ein Python-Programm, das eine beliebige römische Zahl in eine
 „gewöhnliche” Dezimalzahl umrechnet.
 """
 
-r_I = 1
-r_V = 5
-r_X = 10
-r_L = 50
-r_C = 100
-r_D = 500
-r_M = 1000
+#with help of Darkestry's code
+dictionary_roemische_zahlen = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
+while True:
+    print("Restart")
+    input_in_letters = input("Input roman number ").upper()
+    current_digit = 0
+    previous_digit = 0
+    
+    for char in input_in_letters:
+        if char in dictionary_roemische_zahlen:
+            if dictionary_roemische_zahlen[char] > previous_digit:
+                current_digit -= previous_digit
+            else:
+                current_digit += previous_digit
+        previous_digit = dictionary_roemische_zahlen[char]
+    current_digit += previous_digit
 
-if __name__ == "__main__":
-    input = input("Roemische Zahl eingeben:  ")
-    print(input)
-    
-    
+    print("Dezimalzahl: " + str(current_digit))
